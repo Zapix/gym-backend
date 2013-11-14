4c542babbad88b608a250b3ea290e32be80cdb90
Aibulatov Alex <zap.aibulatov@gmail.com>
2013-11-15 04:52:06 +0600

    Add rest read only exercises api

--- gym_backend/exercises/views.py
+++ gym_backend/exercises/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from rest_framework import viewsets
+
+from . import models
+from . import serializers
+
+
+class MuscleGroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.MuscleGroup.objects.all()
+    serializer_class = serializers.MuscleGroupSerializer
+
+
+class ExercisesViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Exercise.objects.all()
+    serializer_class = serializers.ExerciseSerializer

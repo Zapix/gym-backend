9cd2199ab86fc174ffbebcec26e230079418a327
Aibulatov Alex <zap.aibulatov@gmail.com>
2013-11-15 04:01:39 +0600

    Add exercises methods

---
+++ gym_backend/exercises/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class MuscleGroup(models.Model):
+    """
+    Name of muscles group for training
+    """
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_('Muscle`s name')
+    )
+
+    class Meta:
+        verbose_name = _('Muscle group')
+        verbose_name = _('Muscle groups')
+        ordering = ['id']
+
+    def __unicode__(self):
+        if self.pk:
+            return self.name
+        else:
+            return u'New Muscle Group'
+
+
+class Exercise(models.Model):
+    """
+    Information about exercise for muscle group.
+    Storing description and link to youtube video
+    """
+    muscle_group = models.ForeignKey(
+        'MuscleGroup',
+        related_name='exercise_set',
+        verbose_name=_('Muscle group')
+    )
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_('Name')
+    )
+    description = models.TextField(
+        verbose_name=_('Description')
+    )
+    youtube_link = models.URLField(
+        verbose_name=_('Youtube link')
+    )

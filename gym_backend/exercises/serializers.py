3e1336ccdd0b52efe34700aca9a8f1b81e861b7c
Aibulatov Alex <zap.aibulatov@gmail.com>
2013-11-15 12:46:34 +0600

    Add exercise.pk exercise.url for muscle group api

--- gym_backend/exercises/serializers.py
+++ gym_backend/exercises/serializers.py
@@ -1,24 +1,57 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 
 from . import models
 
 
-class MuscleGroupSerializer(serializers.ModelSerializer):
-    exercise_set = serializers.PrimaryKeyRelatedField(
-        many=True,
-        read_only=True
-    )
+class ExerciseRelatedSerializer(serializers.HyperlinkedModelSerializer):
+    pk = serializers.Field('pk')
+
+    class Meta:
+        model = models.Exercise
+        fields = ('pk', 'url', 'name')
+
+
+class ExerciseRelatedField(serializers.RelatedField):
+    read_only = True
+    serializers_class = ExerciseRelatedSerializer
+
+    def to_native(self, value):
+        """
+        Generates json data for exercises.
+        Exercises item include: pk, name, url
+        :param value: exercise value
+        :type value: :class:`exercises.models.Exercise`
+        :return: json object
+        :rtype: str
+        """
+        serializer = self.serializers_class(value)
+        return JSONRenderer().render(serializer.data)
+
+
+class MuscleGroupSerializer(serializers.HyperlinkedModelSerializer):
+    pk = serializers.Field('pk')
+    exercise_set = ExerciseRelatedField(many=True)
 
     class Meta:
         model = models.MuscleGroup
-        fields = ('id', 'name', 'exercise_set')
+        fields = ('url', 'name', 'exercise_set')
 
 
-class ExerciseSerializer(serializers.ModelSerializer):
+class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
+    pk = serializers.Field('pk')
+    muscle_group_pk = serializers.Field('muscle_group.pk')
     muscle_group = serializers.Field('muscle_group.name')
+    markup_link = serializers.HyperlinkedIdentityField(
+        view_name='exercise-markup'
+    )
     markup_description = serializers.Field('markup_description')
 
     class Meta:
         model = models.Exercise
-        fields = ('id', 'name', 'description', 'muscle_group', 'youtube_link')
+        fields = (
+            'pk', 'url', 'name', 'description',
+            'muscle_group', 'muscle_group_pk',
+            'markup_link', 'youtube_link'
+        )

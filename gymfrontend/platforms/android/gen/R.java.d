f999067f516d2b36447ee3c28d6b55396587a004
Aibulatov Alex <zap.aibulatov@gmail.com>
2013-11-19 11:42:53 +0600

    Add angularjs and bootstrap fro codorva project

---
+++ gymfrontend/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/gen/ru/zapix/gymfrontend/R.java \
+ : /Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/drawable/icon.png \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/drawable-hdpi/icon.png \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/drawable-ldpi/icon.png \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/drawable-mdpi/icon.png \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/values/strings.xml \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/res/xml/config.xml \
+/Users/zapix/projects/gym-backend/gymfrontend/platforms/android/bin/AndroidManifest.xml \
---
+++ gymfrontend/platforms/android/gen/ru/zapix/gymfrontend/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.zapix.gymfrontend;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

8ffa60d230e99c475211315d55381f820fe87185
Aibulatov Alex <zap.aibulatov@gmail.com>
2013-11-19 13:02:30 +0600

    Add muscle group list and exercise list

---
+++ gymfrontend/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Created by zapix on 19.11.13.
+ */
+
+gymApp = angular.module('gymApp', []);
+
+gymApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+    $scope.muscleGroups = [];
+    $scope.exerciseList = [];
+
+    $http.get('http://192.168.0.33:8000/api/v1/muscles/').success(function(data){
+        $scope.muscleGroups = data;
+    });
+
+    $http.get('http://192.168.0.33:8000/api/v1/exercises/').success(function(data){
+        $scope.exerciseList = data;
+    });
+
+}]);
